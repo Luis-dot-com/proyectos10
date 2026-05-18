31aa64a1d861b02580faba56bb32a20525e4ae10
Luis <luismarquezjojo@gmail.com>
2026-05-18 11:02:08 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -198,12 +198,14 @@ function agregarTarea() {
     iconos.classList.add('iconos');
     tareaNueva.appendChild(iconos);
 
-    let completar = document.createElement('i');
-    completar.classList.add('bi', 'bi-check-circle-fill', 'icono-completar');
+    let completar = document.createElement('img');
+    completar.setAttribute('src', 'images/check.svg');
+    completar.classList.add('icono-completar')
     completar.addEventListener('click', tareaCompletada)
 
-    let eliminar = document.createElement('i');
-    eliminar.classList.add('bi', 'bi-trash3-fill', 'icono-eliminar');
+    let eliminar = document.createElement('img');
+    eliminar.setAttribute('src', 'images/delete.svg');
+    eliminar.classList.add('icono-eliminar')
     eliminar.addEventListener('click', eliminarTarea)
 
     iconos.append(completar, eliminar);
